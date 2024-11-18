1cf50159ff61925b1971901cc5e973671fb2f3ff
Leslie <Kooliesk@gmail.com>
2024-11-18 22:55:34 +0200

    Add footer styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { fetchAccounts } from '../redux/accountsSlice';
 import { fetchTransactions } from '../redux/transactionsSlice';
 import BalanceSummary from '../features/BalanceSummary';
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,12 @@ const Footer = () => {
     <footer className="bg-indigo-600 text-white py-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center">
-          <p className="text-sm">&copy; 2023 Your Company. All rights reserved.</p>
+          <p className="text-sm">&copy; Bellville Travel Agency. All rights reserved.</p>
           <div className="flex space-x-4">
-            <a href="#" className="text-indigo-200 hover:text-white text-sm">Privacy Policy</a>
-            <a href="#" className="text-indigo-200 hover:text-white text-sm">Terms of Service</a>
-            <a href="#" className="text-indigo-200 hover:text-white text-sm">Contact Us</a>
+            <a href="#home" className="text-indigo-200 hover:text-white text-sm">Home</a>
+            <a href="#accounts" className="text-indigo-200 hover:text-white text-sm">Accounts</a>
+            <a href="#transactions" className="text-indigo-200 hover:text-white text-sm">Transactions</a>
+            <a href="#transfers" className="text-indigo-200 hover:text-white text-sm">Transfers</a>
           </div>
         </div>
       </div>
